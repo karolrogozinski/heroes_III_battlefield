bda2a1e1dd1ced96c607e0b44f112b3c80a31119
Adam Sudoł <01158935@pw.edu.pl>
2022-12-15 22:06:24 +0100

    Corrected run.sh to work on ubuntu

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
-pip3.10 install -r requirements.txt
+#!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pip install -r requirements.txt
+    . ~/.profile
     g++ -O3 -Wall -shared -std=c++17 -fPIC $(python3 -m pybind11 --includes) src/controller.cpp -o src/controller$(python3-config --extension-suffix)
+    python3 ./src/run.py
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+    pip3.10 install -r requirements.txt
     g++ -std=c++17 -shared -undefined dynamic_lookup -I./pybind11/include/ `python3.10 -m pybind11 --includes` src/controller.cpp -o src/controller.so `python3-config --ldflags`
+    python3.10 ./src/run.py
 fi
     
-python3.10 ./src/run.py
+
